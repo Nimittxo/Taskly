96a258d40bdd69b219ffbaa86ba39702e2ccaa71
Nimittxo <akki042005@gmail.com>
2025-03-30 04:51:06 +0530

    Setup

--- App.tsx
+++ App.tsx
@@ -1,11 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View
+        style={
+          styles.itemContainer
+          //borderBottomWidth: 1,
+          //borderBottomColor: "#1a759f",
+          // backgroundColor: "pink",
+          //paddingHorizontal: 8,
+          //paddingVertical: 16,
+        }
+      >
+        <Text style={styles.itemText}>Coffee</Text>
+        <StatusBar style="auto" />
+      </View>
     </View>
   );
 }
@@ -13,8 +24,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    //alignItems: "center",
+    justifyContent: "center",
+  },
+  itemContainer: {
+    borderBottomWidth: 1,
+    borderBottomColor: "#1a759f",
+    //backgroundColor: "pink",
+    paddingHorizontal: 8,
+    paddingVertical: 16,
+  },
+  itemText: {
+    fontSize: 18,
+    fontWeight: "200",
   },
 });
